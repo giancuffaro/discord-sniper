fb23d53b07a261de8ef8bfa2f1c134a74d91f16c
giancuffaro <giancuffaro230@gmail.com>
2026-08-09 14:07:52 -0400

    update from HULK - Sun 08/09/2026 14:07:52.25

--- GET LATEST FROM GITHUB.bat
+++ GET LATEST FROM GITHUB.bat
@@ -3,6 +3,7 @@ cd /d "%~dp0"
 echo ================================================
 echo   Getting the latest from GitHub...
 echo ================================================
+if exist ".git\index.lock" del /f /q ".git\index.lock" >nul 2>&1
 git pull
 echo.
 echo Up to date. You can start working now.
--- SEND CHANGES TO GITHUB.bat
+++ SEND CHANGES TO GITHUB.bat
@@ -3,14 +3,15 @@ cd /d "%~dp0"
 echo ================================================
 echo   Sending your changes up to GitHub...
 echo ================================================
+if exist ".git\index.lock" del /f /q ".git\index.lock" >nul 2>&1
 git add -A
 git commit -m "update from %COMPUTERNAME% - %date% %time%"
-git push
+git push -u origin main
 echo.
 if errorlevel 1 (
   echo If it said REJECTED: double-click "GET LATEST FROM GITHUB" first, then run this again.
 ) else (
-  echo Done - your changes are on GitHub. Your other devices can now Get Latest.
+  echo Done - your changes are on GitHub.
 )
 echo.
 pause
