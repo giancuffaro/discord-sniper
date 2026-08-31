f3971bb9324ad0af532652425c64d4a3fca6d7b4
G <giancuffaro230@gmail.com>
2026-08-31 12:00:07 -0400

    auto-push Mon 08/31/2026 12:00:07.25

--- launch-sniper.bat
+++ launch-sniper.bat
@@ -3,6 +3,8 @@ rem ASCII-named launcher: the desktop shortcut points here, because the
 rem shortcut system can't stomach the emoji in "START HERE.bat". This
 rem finds it by wildcard and hands over.
 cd /d "%~dp0"
+rem breadcrumb (8/29, "icon does nothing"): proves a click reached this far
+>> "launcher-probe.log" echo launcher hit %date% %time%
 for %%f in ("*START HERE.bat") do (
   call "%%~f"
   goto :eof
